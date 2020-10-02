1270950c126990af2b074d852516bbf3d5ffc6c3
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-02 05:46:11 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,25 @@
+class Author 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def posts 
+    Post.all.select{|post| post.author == self}
+  end 
+  
+  def add_post(new_post)
+    new_post.author = self
+  end
+  
+  def add_post_by_title(post_title)
+    new_title = Post.new(post_title)
+    self.add_post(new_title)
+  end 
+  
+  def self.post_count
+    Post.all.count
+  end
+  
+end
